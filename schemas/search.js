8660979dd000a52ce98f373d32034d082e6d217d
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2021-10-29 20:04:25 +0700

    💄 add gitignore

---
+++ schemas/search.js
@@ -0,0 +1,52 @@
+export default {
+    name: 'search',
+    title: 'Search',
+    type: 'document',
+    fields: [
+        {
+          title: "SEO",
+          name: "seo",
+          type: "object",
+          options: {
+            collapsible: true
+          },
+          fields: [
+            {
+              name: "seo_description",
+              type: "string",
+              title: "Description",
+              validation: Rule => Rule.required()
+            },
+            {
+              name: "seo_keywords",
+              type: "string",
+              title: "Keywords",
+              validation: Rule => Rule.required()
+            },
+            {
+              name: "seo_image",
+              title: "Image",
+              description: "800 x 600 | PNG / JPEG / WEBP | max 100kb",
+              type: "image",
+              validation: Rule => Rule.required(),
+              fields: [
+                {
+                  title: "Edit Alt Text",
+                  name: "name",
+                  type: "string",
+                  validation: Rule => Rule.required()
+                },
+              ],
+            },
+          ],
+        },
+    ],
+    preview: {
+      prepare() {
+        return {
+          title: "Search"
+        }
+      }
+    }
+  }
+    
