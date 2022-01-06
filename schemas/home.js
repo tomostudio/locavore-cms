da47592db327807d6e0f6ac942fab2cf9fa1dd12
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-01-06 23:12:19 +0700

    💄 add home seo

--- schemas/home.js
+++ schemas/home.js
@@ -9,6 +9,43 @@ export default {
       type: 'reference',
       to: [{ type: 'issue' }],
     },
+    {
+      title: "SEO",
+      name: "seo",
+      type: "object",
+      options: {
+        collapsible: true
+      },
+      fields: [
+        {
+          name: "seo_description",
+          type: "string",
+          title: "Description",
+          validation: Rule => Rule.required()
+        },
+        {
+          name: "seo_keywords",
+          type: "string",
+          title: "Keywords",
+          validation: Rule => Rule.required()
+        },
+        {
+          name: "seo_image",
+          title: "Image",
+          description: "800 x 600 | PNG / JPEG / WEBP | max 100kb",
+          type: "image",
+          validation: Rule => Rule.required(),
+          fields: [
+            {
+              title: "Edit Alt Text",
+              name: "name",
+              type: "string",
+              validation: Rule => Rule.required()
+            },
+          ],
+        },
+      ],
+    },
   ],
   preview: {
     prepare() {
