fd2228d2468cb98852eda373a686779e1a501eb3
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-03-17 14:41:06 +0700

    🔥 remove default sort order not working

--- deskStructure.js
+++ deskStructure.js
@@ -11,8 +11,9 @@ import {
   FiFlag,
   FiFilter,
   FiAward,
-  FiUsers,
+  FiUsers, 
   FiSliders,
+
 } from 'react-icons/fi'
 
 export default () =>
@@ -68,21 +69,10 @@ export default () =>
           S.list()
             .title('Family')
             .items([
-              S.listItem()
-                .title('Family List')
-                .icon(() => <FiAward />)
-                .child(
-                  S.documentTypeList('family_list')
-                    .title('Family List')
-                    .child(() =>
-                      S.documentList()
-                        .title('Family List')
-                        .defaultOrdering([
-                          { field: 'order', direction: 'asc' },
-                        ]),
-                    ),
-                ),
-              S.documentTypeListItem('member_list').icon(() => <FiUsers />),
+              S.documentTypeListItem('family_list')
+              .icon(() => <FiAward />),
+              S.documentTypeListItem('member_list')
+              .icon(() => <FiUsers />),
             ]),
         ),
       S.listItem()
