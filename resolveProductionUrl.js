40cfb4db095348377a1dbb2f5e293da72dc3453a
Dimas Aditya Mukhsinin <30746931+dimasadityamukhsinin@users.noreply.github.com>
2022-12-16 17:40:49 +0000

    💄 add article preview

---
+++ resolveProductionUrl.js
@@ -0,0 +1,20 @@
+import client from 'part:@sanity/base/client'
+
+let editorMap = new Map();
+
+const fetchDocuments = async () => {
+  const editors = await client.fetch(`*[_type == "issue"]{ _id, slug }`)
+  editors.forEach(editor => editorMap.set(editor._id, editor.slug.current));
+};
+
+/* Run this once on load */
+fetchDocuments();
+
+// ./resolveProductionUrl.js
+export default function resolveProductionUrl(article) {
+  if (!article._type === "article") return null;
+  if (!article.issue) return null;
+  const editorSlug = editorMap.get(article.issue._ref);
+  if (!editorSlug) return null;
+  return `https://locavore-next-phase1.vercel.app/editorial/preview/${editorSlug}/${article.slug.current}`
+}
