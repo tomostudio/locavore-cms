bdb5322d44b993fb36a6e580a697048baa58833f
hendhyhutomo <hendhyhutomo@gmail.com>
2023-10-11 12:38:46 +0700

    Update Page Title Component

--- schemas/nxt/homeNxt.js
+++ schemas/nxt/homeNxt.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react'
 
 // Important items to allow form fields to work properly and patch the dataset.
-import { PatchEvent, set } from "part:@sanity/form-builder/patch-event";
-import FormField from "part:@sanity/components/formfields/default";
+import { PatchEvent, set } from 'part:@sanity/form-builder/patch-event'
+import FormField from 'part:@sanity/components/formfields/default'
 
 // Import the TextArea from UI
-import { TextArea } from "@sanity/ui";
+import { TextArea } from '@sanity/ui'
 
 const inputWithHeight = React.forwardRef((props, ref) => {
-  const { type, onChange } = props;
+  const { type, onChange } = props
   return (
     <FormField label={type.title} description={type.description}>
       <TextArea
@@ -16,365 +16,372 @@ const inputWithHeight = React.forwardRef((props, ref) => {
         placeholder={type.placeholder}
         value={props.value}
         onChange={(event) => {
-          onChange(PatchEvent.from(set(event.target.value)));
+          onChange(PatchEvent.from(set(event.target.value)))
         }}
-        style={{ height: "60px" }}
+        style={{ height: '60px' }}
       />
     </FormField>
-  );
-});
+  )
+})
 
 export default {
-  name: "homeNxt",
-  title: "Home",
-  type: "document",
+  name: 'homeNxt',
+  title: 'Home',
+  type: 'document',
   fields: [
     {
-      title: "SEO",
+      title: 'SEO',
       description:
-        "Search Engine Optimization allows to improve the ranking in search results.",
-      name: "seo",
-      type: "object",
+        'Search Engine Optimization allows to improve the ranking in search results.',
+      name: 'seo',
+      type: 'object',
       options: {
         collapsible: true,
       },
       fields: [
         {
-          name: "seo_description",
+          name: 'seo_description',
           description:
-            "Enter up to 400 characters to describe the page. This description is what will be displayed on search engines or when this page is being shared (e.g. Google or WhatsApp).",
-          type: "string",
-          title: "Description",
+            'Enter up to 400 characters to describe the page. This description is what will be displayed on search engines or when this page is being shared (e.g. Google or WhatsApp).',
+          type: 'string',
+          title: 'Description',
         },
         {
-          name: "seo_keywords",
+          name: 'seo_keywords',
           description:
-            "Enter some keywords to describe the page (separated by commas)",
-          type: "string",
-          title: "Keywords",
+            'Enter some keywords to describe the page (separated by commas)',
+          type: 'string',
+          title: 'Keywords',
         },
         {
-          name: "seo_image",
-          title: "Image",
+          name: 'seo_image',
+          title: 'Image',
           description:
-            "800 x 600 | PNG / JPEG / WEBP | max 100kb. This image is what will be displayed on search engines or when this page is being shared (e.g. Google or WhatsApp)",
-          type: "image",
+            '800 x 600 | PNG / JPEG / WEBP | max 100kb. This image is what will be displayed on search engines or when this page is being shared (e.g. Google or WhatsApp)',
+          type: 'image',
           fields: [
             {
-              title: "Edit Alt Text",
-              name: "alt",
-              type: "string",
-              initialValue: "Locavore NXT",
+              title: 'Edit Alt Text',
+              name: 'alt',
+              type: 'string',
+              initialValue: 'Locavore NXT',
             },
           ],
         },
       ],
     },
     {
-      name: "section1",
-      title: "Landing Section",
-      type: "object",
+      title: 'Page Title',
+      name: 'Page Title',
+      type: 'string',
+      description:
+        'Title for Page Browser',
+    },
+    {
+      name: 'section1',
+      title: 'Landing Section',
+      type: 'object',
       options: {
         collapsible: true,
       },
       fields: [
         {
-          name: "imageDesktop",
-          title: "Image Desktop",
-          type: "image",
-          description: "Input Desktop Homepage Hero Image: 1440 x 900 px",
+          name: 'imageDesktop',
+          title: 'Image Desktop',
+          type: 'image',
+          description: 'Input Desktop Homepage Hero Image: 1440 x 900 px',
           validation: (Rule) =>
             Rule.custom((field, _) => {
-              return field?.asset ? true : "Required";
+              return field?.asset ? true : 'Required'
             }),
           fields: [
             {
-              title: "Edit Alt Text",
-              name: "alt",
-              type: "string",
-              initialValue: "Locavore NXT",
+              title: 'Edit Alt Text',
+              name: 'alt',
+              type: 'string',
+              initialValue: 'Locavore NXT',
             },
           ],
         },
         {
-          name: "imageMobile",
-          title: "Image Mobile",
-          type: "image",
-          description: "Input Mobile Homepage Hero Image: 375 x 870 px",
+          name: 'imageMobile',
+          title: 'Image Mobile',
+          type: 'image',
+          description: 'Input Mobile Homepage Hero Image: 375 x 870 px',
           validation: (Rule) =>
             Rule.custom((field, _) => {
-              return field?.asset ? true : "Required";
+              return field?.asset ? true : 'Required'
             }),
           fields: [
             {
-              title: "Edit Alt Text",
-              name: "alt",
-              type: "string",
-              initialValue: "Locavore NXT",
+              title: 'Edit Alt Text',
+              name: 'alt',
+              type: 'string',
+              initialValue: 'Locavore NXT',
             },
           ],
         },
         {
-          title: "Description",
-          name: "description",
-          type: "string",
+          title: 'Description',
+          name: 'description',
+          type: 'string',
           description:
-            "Short description about the new Locavore (1-2 sentences or 15-20 words)",
+            'Short description about the new Locavore (1-2 sentences or 15-20 words)',
           inputComponent: inputWithHeight,
           validation: (Rule) => Rule.required(),
         },
         {
-          title: "Text Left",
-          name: "textLeft",
-          type: "string",
-          description: "Input keywords (max 2 words)",
+          title: 'Text Left',
+          name: 'textLeft',
+          type: 'string',
+          description: 'Input keywords (max 2 words)',
         },
         {
-          title: "Text Right",
-          name: "textRight",
-          type: "string",
-          description: "Input keywords (max 2 words)",
+          title: 'Text Right',
+          name: 'textRight',
+          type: 'string',
+          description: 'Input keywords (max 2 words)',
         },
       ],
     },
     {
-      name: "section2",
-      title: "Menu Section",
-      type: "object",
+      name: 'section2',
+      title: 'Menu Section',
+      type: 'object',
       options: {
         collapsible: true,
       },
       fields: [
         {
-          name: "option",
-          title: "Layout Options",
-          type: "string",
+          name: 'option',
+          title: 'Layout Options',
+          type: 'string',
           options: {
             list: [
-              { title: "Option 1", value: "option1" },
-              { title: "Option 2", value: "option2" },
-              { title: "Option 3", value: "option3" },
+              { title: 'Option 1', value: 'option1' },
+              { title: 'Option 2', value: 'option2' },
+              { title: 'Option 3', value: 'option3' },
             ],
-            layout: "radio",
+            layout: 'radio',
           },
           validation: (Rule) => Rule.required(),
-          initialValue: "option1",
+          initialValue: 'option1',
         },
         {
-          title: "Image Set (For Option 1)",
-          name: "imageOption1",
-          type: "object",
-          hidden: ({ parent }) => !(parent?.option === "option1"),
+          title: 'Image Set (For Option 1)',
+          name: 'imageOption1',
+          type: 'object',
+          hidden: ({ parent }) => !(parent?.option === 'option1'),
           fields: [
             {
-              title: "Image Desktop",
-              name: "imageDesktop",
-              type: "image",
+              title: 'Image Desktop',
+              name: 'imageDesktop',
+              type: 'image',
               validation: (Rule) =>
                 Rule.custom((field, context) => {
-                  if (context.document.section2.option === "option1") {
-                    return field ? true : "Required";
+                  if (context.document.section2.option === 'option1') {
+                    return field ? true : 'Required'
                   } else {
-                    return true;
+                    return true
                   }
                 }),
               description:
-                "Desktop Homepage Menu Image Layout Option 1: 1440 x 900 px",
+                'Desktop Homepage Menu Image Layout Option 1: 1440 x 900 px',
               fields: [
                 {
-                  title: "Edit Alt Text",
-                  name: "alt",
-                  type: "string",
-                  initialValue: "Locavore NXT",
+                  title: 'Edit Alt Text',
+                  name: 'alt',
+                  type: 'string',
+                  initialValue: 'Locavore NXT',
                 },
               ],
             },
             {
-              title: "Image Mobile",
-              name: "imageMobile",
-              type: "image",
+              title: 'Image Mobile',
+              name: 'imageMobile',
+              type: 'image',
               validation: (Rule) =>
                 Rule.custom((field, context) => {
-                  if (context.document.section2.option === "option1") {
-                    return field ? true : "Required";
+                  if (context.document.section2.option === 'option1') {
+                    return field ? true : 'Required'
                   } else {
-                    return true;
+                    return true
                   }
                 }),
               description:
-                "Mobile Homepage Menu Image Layout Option 1: 375 x 872 px",
+                'Mobile Homepage Menu Image Layout Option 1: 375 x 872 px',
               fields: [
                 {
-                  title: "Edit Alt Text",
-                  name: "alt",
-                  type: "string",
-                  initialValue: "Locavore NXT",
+                  title: 'Edit Alt Text',
+                  name: 'alt',
+                  type: 'string',
+                  initialValue: 'Locavore NXT',
                 },
               ],
             },
           ],
         },
         {
-          title: "Image (For Option 2)",
-          name: "imageOption2",
-          type: "image",
-          hidden: ({ parent }) => !(parent?.option === "option2"),
+          title: 'Image (For Option 2)',
+          name: 'imageOption2',
+          type: 'image',
+          hidden: ({ parent }) => !(parent?.option === 'option2'),
           validation: (Rule) =>
             Rule.custom((field, context) => {
-              if (context.document.section2.option === "option2") {
-                return field ? true : "Required";
+              if (context.document.section2.option === 'option2') {
+                return field ? true : 'Required'
               } else {
-                return true;
+                return true
               }
             }),
           description:
-            "Desktop Homepage Menu Image Layout Option 2: 800 x 435 px",
+            'Desktop Homepage Menu Image Layout Option 2: 800 x 435 px',
           fields: [
             {
-              title: "Edit Alt Text",
-              name: "alt",
-              type: "string",
-              initialValue: "Locavore NXT",
+              title: 'Edit Alt Text',
+              name: 'alt',
+              type: 'string',
+              initialValue: 'Locavore NXT',
             },
           ],
         },
         {
-          title: "Image Set (For Option 3)",
-          name: "imageOption3",
-          type: "object",
-          hidden: ({ parent }) => !(parent?.option === "option3"),
+          title: 'Image Set (For Option 3)',
+          name: 'imageOption3',
+          type: 'object',
+          hidden: ({ parent }) => !(parent?.option === 'option3'),
           fields: [
             {
-              title: "Image Left",
-              name: "imageLeft",
-              type: "image",
+              title: 'Image Left',
+              name: 'imageLeft',
+              type: 'image',
               validation: (Rule) =>
                 Rule.custom((field, context) => {
-                  if (context.document.section2.option === "option3") {
-                    return field ? true : "Required";
+                  if (context.document.section2.option === 'option3') {
+                    return field ? true : 'Required'
                   } else {
-                    return true;
+                    return true
                   }
                 }),
               description:
-                "Desktop Homepage Menu Image Layout Option 2: 384 x 210 px",
+                'Desktop Homepage Menu Image Layout Option 2: 384 x 210 px',
               fields: [
                 {
-                  title: "Edit Alt Text",
-                  name: "alt",
-                  type: "string",
-                  initialValue: "Locavore NXT",
+                  title: 'Edit Alt Text',
+                  name: 'alt',
+                  type: 'string',
+                  initialValue: 'Locavore NXT',
                 },
               ],
             },
             {
-              title: "Image Right",
-              name: "imageRight",
-              type: "image",
+              title: 'Image Right',
+              name: 'imageRight',
+              type: 'image',
               validation: (Rule) =>
                 Rule.custom((field, context) => {
-                  if (context.document.section2.option === "option3") {
+                  if (context.document.section2.option === 'option3') {
                     console.log(field)
-                    return field ? true : "Required";
+                    return field ? true : 'Required'
                   } else {
-                    return true;
+                    return true
                   }
                 }),
               description:
-                "Desktop Homepage Menu Image Layout Option 2: 384 x 210 px",
+                'Desktop Homepage Menu Image Layout Option 2: 384 x 210 px',
               fields: [
                 {
-                  title: "Edit Alt Text",
-                  name: "alt",
-                  type: "string",
-                  initialValue: "Locavore NXT",
+                  title: 'Edit Alt Text',
+                  name: 'alt',
+                  type: 'string',
+                  initialValue: 'Locavore NXT',
                 },
               ],
             },
           ],
         },
         {
-          title: "Heading",
-          name: "heading",
-          type: "string",
+          title: 'Heading',
+          name: 'heading',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
         {
-          title: "Menu Title",
-          name: "menuTitle",
-          type: "string",
+          title: 'Menu Title',
+          name: 'menuTitle',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
         {
-          title: "Button Text",
-          name: "button",
-          type: "string",
-          validation: (Rule) => Rule.required(),
-        },
-      ],
-    },
-    {
-      name: "facilitiesSection",
-      title: "Our Facilities Section",
-      type: "object",
-      options: {
-        collapsible: true,
-      },
-      fields: [
-        {
-          title: "Heading",
-          name: "heading",
-          type: "string",
-          validation: (Rule) => Rule.required(),
-        },
-        {
-          title: "Button Text",
-          name: "button",
-          type: "string",
+          title: 'Button Text',
+          name: 'button',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
       ],
     },
     {
-      name: "collaboratorSection",
-      title: "Our Collaborators Section",
-      type: "object",
+      name: 'facilitiesSection',
+      title: 'Our Facilities Section',
+      type: 'object',
       options: {
         collapsible: true,
       },
       fields: [
         {
-          title: "Heading",
-          name: "heading",
-          type: "string",
+          title: 'Heading',
+          name: 'heading',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
         {
-          title: "Button Text",
-          name: "button",
-          type: "string",
+          title: 'Button Text',
+          name: 'button',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
       ],
     },
     {
-      name: "eventSection",
-      title: "Our Events & Programs Section",
-      type: "object",
+      name: 'collaboratorSection',
+      title: 'Our Collaborators Section',
+      type: 'object',
       options: {
         collapsible: true,
       },
       fields: [
         {
-          title: "Heading",
-          name: "heading",
-          type: "string",
+          title: 'Heading',
+          name: 'heading',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
         {
-          title: "Button Text",
-          name: "button",
-          type: "string",
+          title: 'Button Text',
+          name: 'button',
+          type: 'string',
+          validation: (Rule) => Rule.required(),
+        },
+      ],
+    },
+    {
+      name: 'eventSection',
+      title: 'Our Events & Programs Section',
+      type: 'object',
+      options: {
+        collapsible: true,
+      },
+      fields: [
+        {
+          title: 'Heading',
+          name: 'heading',
+          type: 'string',
+          validation: (Rule) => Rule.required(),
+        },
+        {
+          title: 'Button Text',
+          name: 'button',
+          type: 'string',
           validation: (Rule) => Rule.required(),
         },
       ],
@@ -383,8 +390,8 @@ export default {
   preview: {
     prepare() {
       return {
-        title: "Home",
-      };
+        title: 'Home',
+      }
     },
   },
-};
+}
