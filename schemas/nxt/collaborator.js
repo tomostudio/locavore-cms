155904327d2fd62e5c44e12e44441d8d38c9d09b
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2023-10-11 10:08:57 +0700

    🐛 fix bug validation image required collaborator

--- schemas/nxt/collaborator.js
+++ schemas/nxt/collaborator.js
@@ -54,6 +54,10 @@ export default {
           title: "Image Desktop",
           type: "image",
           description: "Hero Image Size: 1440 x 350 px",
+          validation: (Rule) =>
+            Rule.custom(({ asset }) => {
+              return asset ? true : "Required";
+            }),
           fields: [
             {
               title: "Edit Alt Text",
@@ -68,6 +72,10 @@ export default {
           title: "Image Mobile",
           type: "image",
           description: "Hero Image Size: 375 x 350 px",
+          validation: (Rule) =>
+            Rule.custom(({ asset }) => {
+              return asset ? true : "Required";
+            }),
           fields: [
             {
               title: "Edit Alt Text",
