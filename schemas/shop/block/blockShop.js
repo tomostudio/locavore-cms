4648403bdfc9172ab8def058d83e82a4bd23b0b0
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2024-04-26 12:06:12 +0700

    💄 add: link on text editor shop

--- schemas/shop/block/blockShop.js
+++ schemas/shop/block/blockShop.js
@@ -1,3 +1,14 @@
+import React from "react";
+import { HiOutlineLink } from 'react-icons/hi'
+
+const annotationRender = (props, icon) => (
+  <span>
+    {props.children}&nbsp;
+    {icon}
+    &nbsp;
+  </span>
+)
+
 export default {
   title: "Block Shop Description",
   name: "blockShop",
@@ -11,12 +22,33 @@ export default {
           { title: "Strong", value: "strong" },
           { title: "Emphasis", value: "em" },
           { title: "Strike", value: "strike-through" },
-          { title: "Underline", value: "underline" }
+          { title: "Underline", value: "underline" },
         ],
-        annotations: [],
+        annotations: [
+          {
+            title: "Link",
+            name: "link",
+            type: "object",
+            blockEditor: {
+              icon: () => <HiOutlineLink />,
+              render: (props) => annotationRender(props, <HiOutlineLink />),
+            },
+            fields: [
+              {
+                title: "URL",
+                name: "url",
+                type: "url",
+                validation: (Rule) =>
+                  Rule.uri({
+                    scheme: ["http", "https"],
+                  }).required(),
+              },
+            ],
+          },
+        ],
       },
       styles: [],
       lists: [],
-    }
+    },
   ],
 };
