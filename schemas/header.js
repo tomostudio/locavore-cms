45e763fb90a78da6031e8a63a693761dd39876b7
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2024-08-29 12:00:53 +0700

    💄 add: book now

--- schemas/header.js
+++ schemas/header.js
@@ -1,87 +1,112 @@
 export default {
-  name: 'header',
-  title: 'Header',
-  type: 'document',
+  name: "header",
+  title: "Header",
+  type: "document",
   fields: [
     {
-      name: 'logo',
-      title: 'Logo',
-      type: 'object',
+      name: "logo",
+      title: "Logo",
+      type: "object",
       fields: [
         {
-          name: 'white',
-          description: 'PNG / WEBP',
-          title: 'White',
-          type: 'image',
+          name: "white",
+          description: "PNG / WEBP",
+          title: "White",
+          type: "image",
           validation: (Rule) =>
             Rule.custom((field, _) => {
               return field?.asset ? true : "Required";
             }),
           fields: [
             {
-              title: 'Edit Alt Text',
-              name: 'name',
-              type: 'string',
-              initialValue: 'Locavore NXT',
+              title: "Edit Alt Text",
+              name: "name",
+              type: "string",
+              initialValue: "Locavore NXT",
             },
           ],
         },
         {
-          name: 'black',
-          description: 'PNG / WEBP',
-          title: 'Black',
-          type: 'image',
+          name: "black",
+          description: "PNG / WEBP",
+          title: "Black",
+          type: "image",
           validation: (Rule) =>
             Rule.custom((field, _) => {
               return field?.asset ? true : "Required";
             }),
           fields: [
             {
-              title: 'Edit Alt Text',
-              name: 'name',
-              type: 'string',
-              initialValue: 'Locavore NXT',
+              title: "Edit Alt Text",
+              name: "name",
+              type: "string",
+              initialValue: "Locavore NXT",
             },
           ],
         },
       ],
     },
     {
-      title: 'Header Options',
-      description: 'Items listed here will be displayed on the website header',
-      name: 'headerLink',
-      type: 'array',
+      title: "Header Options",
+      description: "Items listed here will be displayed on the website header",
+      name: "headerLink",
+      type: "array",
       of: [
         {
-          title: 'Link',
-          name: 'link',
-          type: 'object',
+          title: "Link",
+          name: "link",
+          type: "object",
           fields: [
             {
-              title: 'Title',
-              name: 'title',
-              type: 'string',
+              title: "Title",
+              name: "title",
+              type: "string",
             },
             {
-              title: 'Link',
-              name: 'link',
-              type: 'string',
+              title: "Link",
+              name: "link",
+              type: "string",
             },
           ],
         },
       ],
     },
     {
-      title: 'Booking',
-      name: 'booking',
-      type: 'url',
-    },
+      title: "Book Now",
+      name: "bookNow",
+      type: "array",
+      validation: (Rule) => Rule.required().min(1),
+      of: [
+        {
+          title: "Book",
+          name: "book",
+          type: "object",
+          fields: [
+            {
+              title: "Name",
+              name: "name",
+              type: "string",
+              validation: (Rule) => Rule.required(),
+            },
+            {
+              title: "Link",
+              name: "link",
+              type: "url",
+              validation: (Rule) =>
+                Rule.uri({
+                  scheme: ["http", "https"],
+                }).required(),
+            },
+          ],
+        },
+      ],
+    }
   ],
   preview: {
     prepare() {
       return {
-        title: 'Header',
-      }
+        title: "Header",
+      };
     },
   },
-}
+};
